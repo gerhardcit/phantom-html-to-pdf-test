6db1bcf381137e12d788fff479fddd1af14fa2e8
Gerhard Pretorius <gerhardcit@gmail.com>
2017-04-24 20:22:58 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+var conversion = require("phantom-html-to-pdf")();
+
+var app = express();
+
+app.get('/', function (req, res) {
+    console.log("html-pdf-test");
+    res.send("html-pdf-test")
+});
+
+app.get('/html', function (req, res) {
+    console.log("test html5");
+    fs.readFile("./test.html", function (err, html) {
+        res.header("Content-Type", "text/html");
+        res.send(html);
+    })
+});
+
+app.get('/pdf', function (req, res) {
+
+    fs.readFile("./test.html", function (err, html) {
+        conversion({ html: "html" }, function (err, pdf) {
+            console.log(pdf.logs);
+            console.log(pdf.numberOfPages);
+            res.header("Content-Type", "application/pdf");
+            pdf.stream.pipe(res);
+        });
+    })
+});
+
+var port = 17455;
+http.createServer(app).listen(port, '0.0.0.0', function () {
+    process.on('uncaughtException', function (error) {
+        console.log("ERROR | UNCAUGHT | " + new Date() + " | " + error.stack);
+        console.log("===============================================================================================================\n");
+    });
+});
+
+
